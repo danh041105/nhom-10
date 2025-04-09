9691302d0b7fef48fdeb9fbaf3f29abdad485839
danh041105 <duyanhn519@gmail.com>
2025-04-09 17:45:32 +0700

    change

---
+++ cau 1/README.md
@@ -0,0 +1,4 @@
+Lưu ý:
+-) đầu tiên hãy connect file qlsv.sql trên mysql workbench để có thể kết nối được database
+
+-) để có thể hiện thị giao diện web hãy chạy file app.py sau đó sẽ hiện đường dẫn bấm vào là web sẽ hiện ra
--- cau 1/qlsv.sql
+++ cau 1/qlsv.sql
@@ -1,1 +1,10 @@
+CREATE DATABASE student_management;
+CREATE TABLE IF NOT EXISTS student_management.students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fullName VARCHAR(255) NOT NULL,
+    studentId VARCHAR(20) NOT NULL UNIQUE,
+    birthDate DATE NOT NULL,
+    hometown VARCHAR(255) NOT NULL
+)
+
 SELECT * FROM student_management.students;
